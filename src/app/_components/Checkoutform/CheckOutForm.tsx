e0c0ae601134f97f4fbe51a12cbf851193177d4c
Samasameh9 <samaasamehh24@gmail.com>
2026-02-14 19:40:48 +0200

    .

--- src/app/_components/Checkoutform/CheckOutForm.tsx
+++ src/app/_components/Checkoutform/CheckOutForm.tsx
@@ -37,7 +37,7 @@ export default function CheckOutForm({cartId}:{cartId:string}) {
   let response= await PayOnlineOrders(cartId ,shippingAddress )
   console.log(response);
 if(response.status=='success'){
-  window.location.href='/Allorders'
+  window.location.href=response.session.url
 }else{
    toast.error("Error")
 }
